10e182428e24b3a402ddad6c869c324d9aec07fb
BiggaHD <dzenis.hankusic@gmail.com>
2019-08-22 21:48:10 +0200

    XO

--- config/db.js
+++ config/db.js
@@ -4,7 +4,7 @@ const db = require("./keys");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(db, {
+    await mongoose.connect(db.mongoURI, {
       useNewUrlParser: true,
       useCreateIndex: true,
       useFindAndModify: false
