aaa07c0797970a5cee313d1c84e5eb37313c4212
BiggaHD <dzenis.hankusic@gmail.com>
2019-08-22 21:39:00 +0200

    XO

---
+++ config/keys_dev.js
@@ -0,0 +1,6 @@
+module.exports = {
+  mongoURI: "mongodb://Bigga_HD_2:Test_321@ds223605.mlab.com:23605/dev-nexu",
+  jwtSecret: "blahblahblah",
+  githubClientId: "0abc055115044ce8a318",
+  githubSecret: "2b0463363157d4860561e0f19d46248791be29e4"
+};
