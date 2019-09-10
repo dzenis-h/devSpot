1c51d6c0968abecceb1e5355b150b32b0e643655
BiggaHD <dzenis.hankusic@gmail.com>
2019-09-10 03:00:07 +0200

    Night shift :)

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,6 +1,6 @@
 module.exports = {
-  mongoURI: process.env.MONGO_URI,
-  jwtSecret: process.env.JWT_SECRET,
-  githubClientId: process.env.GITHUB_ID,
-  githubSecret: process.env.GITHUB_SECRET
+  mongoURI: process.env.MONGO_URI, // as the name says -Here is the place for your MOngoDB URI string
+  jwtSecret: process.env.JWT_SECRET, // put whatever STRING you want as this value. The longer and more diverse it is the more secure :)
+  githubClientId: process.env.GITHUB_ID, // https://auth0.com/docs/connections/social/github
+  githubSecret: process.env.GITHUB_SECRET // https://auth0.com/docs/connections/social/github
 };
